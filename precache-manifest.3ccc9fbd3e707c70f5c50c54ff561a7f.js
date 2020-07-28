764ed259a667b4498af62f0b8dd56defd419ef31
SubinAdhikari <subinadhikari61@gmail.com>
2020-07-28 18:28:47 +0545

    Updates

---
+++ precache-manifest.3ccc9fbd3e707c70f5c50c54ff561a7f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b855138316bcb704f0e832a9fe1a0f5a",
+    "url": "/npmjshomepageclone/index.html"
+  },
+  {
+    "revision": "f647f48c2cb60e0df0d1",
+    "url": "/npmjshomepageclone/static/css/main.d2436648.chunk.css"
+  },
+  {
+    "revision": "338b208c01e5a2022f24",
+    "url": "/npmjshomepageclone/static/js/2.f073e8f9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/npmjshomepageclone/static/js/2.f073e8f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f647f48c2cb60e0df0d1",
+    "url": "/npmjshomepageclone/static/js/main.9a39c64d.chunk.js"
+  },
+  {
+    "revision": "54dfdc97c326f060aced",
+    "url": "/npmjshomepageclone/static/js/runtime-main.8e322296.js"
+  },
+  {
+    "revision": "f3bf7aa51966ff5f6ef4b2b9be0ddd3c",
+    "url": "/npmjshomepageclone/static/media/aboutus.f3bf7aa5.png"
+  },
+  {
+    "revision": "76883a513321229ebefdc3d72c7f0019",
+    "url": "/npmjshomepageclone/static/media/companies.76883a51.png"
+  },
+  {
+    "revision": "1cd2de8f8fce4ae31575ce3b654f0dac",
+    "url": "/npmjshomepageclone/static/media/npmlogoimg-svg.1cd2de8f.svg"
+  },
+  {
+    "revision": "a155adb14e6efc556166663a34d4c0ab",
+    "url": "/npmjshomepageclone/static/media/npmlogoimg.a155adb1.svg"
+  }
+]);
